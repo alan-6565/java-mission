de025f228c5eebc50b2a5554b622cec3015697d2
alan-6565 <153258467+alan-6565@users.noreply.github.com>
2025-11-26 10:16:09 -0800

    Added my bucket list item

--- src/main/java/com/mission/Main.java
+++ src/main/java/com/mission/Main.java
@@ -2,7 +2,7 @@ package com.mission;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Fun Fact:");
+        System.out.println("Bucket List:");
         System.out.println("i would like to travel to japan");
     }
 }

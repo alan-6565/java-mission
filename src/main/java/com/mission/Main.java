093590d72582778fa3aa0f8b774ecd119b328caa
alan-6565 <153258467+alan-6565@users.noreply.github.com>
2025-11-26 09:29:52 -0800

    Added my personal fun fact

--- src/main/java/com/mission/Main.java
+++ src/main/java/com/mission/Main.java
@@ -2,6 +2,7 @@ package com.mission;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("My fun fact:");
+        System.out.println("i collect lots of starwars and spiderman items to much maybe");
     }
 }
